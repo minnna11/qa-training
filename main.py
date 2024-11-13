141cdecd7414092b932ee758d600da03bbaa77ab
Min <79928641+minnna11@users.noreply.github.com>
2024-11-13 18:37:04 +0000

    Created terminal_script to execute all command in terminal

---
+++ main.py
@@ -0,0 +1,8 @@
+import database
+
+# Create the database if it doesn't exist
+database.create_database()
+
+database.create_user()
+
+database.print_database()
---
+++ terminal_script.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+#This is a shell script
+#You can write terminal commands in here and they will all execute when you run this script
+#to call this script, type: chmod 755 ./terminal_script.sh and then ./terminal_script.sh
+echo Start shell script
+ls
+pwd
+echo End shell script
